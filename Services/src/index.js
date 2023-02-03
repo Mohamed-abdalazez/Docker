05f07ea9e92a56bbb6c30d41ffc6f5f0008c55d7
Mohamed-abdalazez <mohamed.abdalazez818@gmail.com>
2023-02-03 03:40:10 +0200

    add redis

--- Services/src/index.js
+++ Services/src/index.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const redis = require("redis");
 
 // init app
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+// connect to redis
+
+const REDIS_PORT = 6379;
+const REDIS_HOST = "redis";
+const redisClient = redis.createClient({
+  //url: "redis://host:port",
+  url: `redis://${REDIS_HOST}:${REDIS_PORT}`,
+});
+redisClient.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on("connect", () => console.log("Connected to redis..."));
+redisClient.connect();
 
 //connect db
 
@@ -19,8 +32,15 @@ mongoose
   .then(() => console.log("connected to db..."))
   .catch((err) => console.log("failed to connect to db: ", err));
 
-app.get("/", (req, res) =>
-  res.send("<h1>Hi from Docker with MongoDB & NodeJS & Mongo-Express</h1>")
-);
+// just for try
+app.get("/", (req, res) => {
+  redisClient.set("Anime", "Naruto...");
+  res.send("<h1>Hi guess who i am :D</h1>");
+});
+
+app.get("/data", async (req, res) => {
+  const anime = await redisClient.get("Anime");
+  res.send(`<h1>Hi from Redis Server</h1><h2>And I'm ${anime}</h2>`);
+});
 
 app.listen(PORT, () => console.log(`Hi Hi! from ${PORT}`));
